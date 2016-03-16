44e4270f55845e0208b5a0d4e117285115238ecb
Alex Poliakov <apoliakov@paradigm4.com>
2016-03-16 14:04:57 -0400

    Fix test

--- src/test.sh
+++ src/test.sh
@@ -1,34 +1,28 @@
 #!/bin/bash
 
 iquery -anq "remove(zero_to_255)"                > /dev/null 2>&1
-
 iquery -anq "store( build( <val:string> [x=0:255,10,0],  string(x % 256) ), zero_to_255 )"  > /dev/null 2>&1
-
-iquery -anq "store( build( <val:string> [x=0:255,10,5],  string(x % 256) ), zero_to_255_overlap )"  > /dev/null 2>&1
 
 rm test.out
 rm test.expected
 
 touch ./test.expected
-
-
-iquery -o csv -aq "limit(zero_to_255)" >> test.out
-
-echo 'count' >> ./test.expected
-echo '256
-' >> ./test.expected
-
-iquery -o csv -aq "limit(between(zero_to_255,0,9))" >> test.out
-
-echo 'count' >> ./test.expected
-echo '10' >> ./test.expected
-
-iquery -o csv -aq "limit(zero_to_255_overlap)" >> test.out
+iquery -o csv:l -aq "op_count(limit(zero_to_255, null))" >> test.out
 echo 'count' >> ./test.expected
 echo '256' >> ./test.expected
 
+iquery -o csv:l -aq "op_count(limit(zero_to_255, 23))" >> test.out
+echo 'count' >> ./test.expected
+echo '23' >> ./test.expected
+
+iquery -o csv:l -aq "op_count(limit(zero_to_255, 7))" >> test.out
+echo 'count' >> ./test.expected
+echo '7' >> ./test.expected
+
+iquery -o csv:l -aq "op_count(limit(zero_to_255, 0))" >> test.out
+echo 'count' >> ./test.expected
+echo '0' >> ./test.expected
 
 diff test.out test.expected
-
 exit 0
 
